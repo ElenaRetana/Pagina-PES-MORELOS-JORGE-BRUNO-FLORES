ad8e34db9c109bbf526504ae636b7c6ff0972cdb
ElenaRetana <83104091+ElenaRetana@users.noreply.github.com>
2021-04-23 21:58:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<center>
+
+    <head>
+        <meta charset="UTF-8">
+        <Title>Inicio de Sesion</Title>
+        <link rel="shortcut icon" href="icon.jpg">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="estilos.css">
+        <style type="text/css">
+
+            {
+                margin: 0px;
+                padding: 0px;
+            }
+            
+            body {
+              background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./imgn/partido.png);
+               height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+               background-size: cover;
+               position: relative;
+   }
+
+            form {
+                background: ;
+                width: 360px;
+                border: 1px solid #4e4d4d;
+                border-radius: 3px;
+                -moz-border-radius: 3px;
+                -webkit-border-radius: 3px;
+                box-shadow: inset 0 0 10px #000000;
+                margin: 100px;
+            }
+            
+            form h1 {
+                text-align: center;
+                color: #fff;
+                font-weight: normal;
+                font-size: 40pt;
+                margin: 30px 0px;
+            }
+            
+            form input {
+                width: 280px;
+                height: 35px;
+                padding: 0px 10px;
+                margin: 10px 30px;
+                color: #6d6d6d;
+                text-align: center;
+            }
+            
+            form button {
+                width: 135px;
+                margin: 20px 0px 30px 30px;
+                height: 25px;
+                /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#2e36a5+12,3cacd8+100 */
+                background: rgb(46, 54, 165);
+                /* Old browsers */
+                background: -moz-linear-gradient(top, rgba(46, 54, 165, 1) 12%, rgba(60, 172, 216, 1) 100%);
+                /* FF3.6-15 */
+                background: -webkit-linear-gradient(top, rgba(46, 54, 165, 1) 12%, rgba(60, 172, 216, 1) 100%);
+                /* Chrome10-25,Safari5.1-6 */
+                background: linear-gradient(to bottom, rgba(46, 54, 165, 1) 12%, rgba(60, 172, 216, 1) 100%);
+                /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+                filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#2e36a5', endColorstr='#3cacd8', GradientType=0);
+                /* IE6-9 */
+                border: 1px solid #232323;
+                color: #fff;
+                box-shadow: 0px 2px 0px #000000;
+                -moz-box-shadow: 0px 2px 0px #000000;
+                -webkit-box-shadow: 0px 2px 0px #000000;
+                border-radius: 3px;
+                -moz-boder-radius: 3px;
+                -webkit-border-radius: 3px;
+            }
+            
+            form button:hover {
+                background: rgb(45, 179, 4);
+            }
+            
+            form button:active {
+                /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#b8e1fc+0,a9d2f3+10,90bae4+25,90bcea+37,90bff0+50,6ba8e5+51,a2daf5+83,bdf3fd+100;Blue+Gloss+%231 */
+                background: rgb(184, 225, 252);
+                /* Old browsers */
+                background: -moz-linear-gradient(top, rgba(184, 225, 252, 1) 0%, rgba(169, 210, 243, 1) 10%, rgba(144, 186, 228, 1) 25%, rgba(144, 188, 234, 1) 37%, rgba(144, 191, 240, 1) 50%, rgba(107, 168, 229, 1) 51%, rgba(162, 218, 245, 1) 83%, rgba(189, 243, 253, 1) 100%);
+                /* FF3.6-15 */
+                background: -webkit-linear-gradient(top, rgba(184, 225, 252, 1) 0%, rgba(169, 210, 243, 1) 10%, rgba(144, 186, 228, 1) 25%, rgba(144, 188, 234, 1) 37%, rgba(144, 191, 240, 1) 50%, rgba(107, 168, 229, 1) 51%, rgba(162, 218, 245, 1) 83%, rgba(189, 243, 253, 1) 100%);
+                /* Chrome10-25,Safari5.1-6 */
+                background: linear-gradient(to bottom, rgba(184, 225, 252, 1) 0%, rgba(169, 210, 243, 1) 10%, rgba(144, 186, 228, 1) 25%, rgba(144, 188, 234, 1) 37%, rgba(144, 191, 240, 1) 50%, rgba(107, 168, 229, 1) 51%, rgba(162, 218, 245, 1) 83%, rgba(189, 243, 253, 1) 100%);
+                /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+                filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='#b8e1fc', endColorstr='#bdf3fd', GradientType=0);
+                /* IE6-9 */
+            }
+        </style>
+    </head>
+
+    <body>
+        <form>
+            <h1>PES - JORGE BRUNO FLORES 2021.</h1>
+            <button  type="submit" class="btn" name="registro">
+                <a style="text-decoration:none" href="pes.html">ENTRAR</a> 
+               </button>
+        </form>
+    </body>
+</html>
